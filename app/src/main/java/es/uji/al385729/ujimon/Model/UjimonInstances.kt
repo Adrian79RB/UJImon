ff474ff5b36fc98840f7df673eeedd9a2eeb4b8a
ivan <al385762@uji.es>
2021-05-25 18:03:46 +0200

    Functions that creates enemyTeams and modified UjimonInstnces

--- app/src/main/java/es/uji/al385729/ujimon/Model/UjimonInstances.kt
+++ app/src/main/java/es/uji/al385729/ujimon/Model/UjimonInstances.kt
@@ -3,13 +3,41 @@ package es.uji.al385729.ujimon.Model
 import es.uji.al385729.ujimon.Assets
 
 class UjimonInstances {
-    val maglugInstance = Ujimon(1000f, "Maglug", Assets.maglugAsset, false, Type.FIRE)
-    val obshoInstance = Ujimon(1000f, "Obsho", Assets.obshoAsset, false, Type.DARKNESS)
-    val redashInstance = Ujimon(1000f, "Redash", Assets.redashAsset, false, Type.PLANT)
-    val sworsthInstance = Ujimon(1000f, "Sworsth", Assets.sworsthAsset, false, Type.DARKNESS)
+    val ujimonArray: Array<Ujimon> = Array<Ujimon>(10) {
+        Ujimon(1000f, "Maglug", Assets.maglugAsset, false, Type.FIRE)
+        Ujimon(1000f, "Obsho", Assets.obshoAsset, false, Type.DARKNESS)
+        Ujimon(1000f, "Redash", Assets.redashAsset, false, Type.PLANT)
+        Ujimon(1000f, "Sworsth", Assets.sworsthAsset, false, Type.DARKNESS)
+        Ujimon(1000f, "Maglug", Assets.maglugAsset, false, Type.FIRE)
+        Ujimon(1000f, "Obsho", Assets.obshoAsset, false, Type.DARKNESS)
+        Ujimon(1000f, "Redash", Assets.redashAsset, false, Type.PLANT)
+        Ujimon(1000f, "Sworsth", Assets.sworsthAsset, false, Type.DARKNESS)
+        Ujimon(1000f, "Sworsth", Assets.sworsthAsset, false, Type.DARKNESS)
+    }
 
     init {
-        maglugInstance.ujimonTolerance[0] = Tolerance(Type.DARKNESS, 0.5f)
-        maglugInstance.ujimonTolerance[1] = Tolerance(Type.GROUND, 1.5f)
+        ujimonArray[0].ujimonTolerance[0] = Tolerance(Type.DARKNESS, 0.5f)
+        ujimonArray[0].ujimonTolerance[1] = Tolerance(Type.GROUND, 1.5f)
+        ujimonArray[0].ujimonTolerance[2] = Tolerance(Type.WATER, 1.7f)
+        ujimonArray[0].ujimonTolerance[3] = Tolerance(Type.PLANT, 0.3f)
+        ujimonArray[0].ujimonTolerance[4] = Tolerance(Type.FIRE, 1f)
+
+        ujimonArray[1].ujimonTolerance[0] = Tolerance(Type.DARKNESS, 1f)
+        ujimonArray[1].ujimonTolerance[1] = Tolerance(Type.GROUND,0.5f)
+        ujimonArray[1].ujimonTolerance[2] = Tolerance(Type.WATER, 1.3f)
+        ujimonArray[1].ujimonTolerance[3] = Tolerance(Type.PLANT, 0.3f)
+        ujimonArray[1].ujimonTolerance[4] = Tolerance(Type.FIRE, 1.7f)
+
+        ujimonArray[2].ujimonTolerance[0] = Tolerance(Type.DARKNESS, 1.8f)
+        ujimonArray[2].ujimonTolerance[1] = Tolerance(Type.GROUND, 0.1f)
+        ujimonArray[2].ujimonTolerance[2] = Tolerance(Type.WATER, 0.5f)
+        ujimonArray[2].ujimonTolerance[3] = Tolerance(Type.PLANT, 1f)
+        ujimonArray[2].ujimonTolerance[4] = Tolerance(Type.FIRE, 1.5f)
+
+        ujimonArray[3].ujimonTolerance[0] = Tolerance(Type.DARKNESS, 1f)
+        ujimonArray[3].ujimonTolerance[1] = Tolerance(Type.GROUND , 0.2f)
+        ujimonArray[3].ujimonTolerance[2] = Tolerance(Type.WATER, 1.3f)
+        ujimonArray[3].ujimonTolerance[3] = Tolerance(Type.PLANT, 0.6f)
+        ujimonArray[3].ujimonTolerance[4] = Tolerance(Type.FIRE, 1.8f)
     }
 }
--- app/src/main/java/es/uji/al385729/ujimon/Model/UjimonModel.kt
+++ app/src/main/java/es/uji/al385729/ujimon/Model/UjimonModel.kt
@@ -2,6 +2,7 @@ package es.uji.al385729.ujimon.Model
 
 import android.widget.TextView
 import es.uji.al385729.ujimon.Assets
+import kotlin.random.Random
 
 class UjimonModel(val playerTrainer : Trainer, val enemyTrainer1 : Trainer, val enemyTrainer2: Trainer, val enemyTrainer3 : Trainer, val soundPlayer : SoundPlayer) {
 
@@ -29,15 +30,43 @@ class UjimonModel(val playerTrainer : Trainer, val enemyTrainer1 : Trainer, val 
     var lastState = UjimonState.UJIMON_SELECTION
     val ujimonInstances = UjimonInstances()
 
+
+
     fun changeModelState(newState : UjimonState) {
         lastState = state
         state = newState
     }
 
-    fun initializeUjimon(){
-        var maglugInstance : Ujimon = Ujimon(1000f,"Maglug", Assets.maglugAsset, false, Type.FIRE)
-        var obshoInstance : Ujimon = Ujimon(1000f, "Obsho", Assets.obshoAsset, false, Type.DARKNESS)
-        var redashInstance : Ujimon = Ujimon(1000f, "Redash", Assets.redashAsset, false, Type.PLANT);
-        var sworsthInstance : Ujimon = Ujimon(1000f, "Sworsth", Assets.sworsthAsset, false, Type.DARKNESS);
+    fun createEnemyTrainer1Team(){
+        var selectedUjimon : Int
+        for( i in 0 until 2){
+            do {
+                selectedUjimon = Random.nextInt(0,9)
+            }
+            while (ujimonInstances.ujimonArray[selectedUjimon].name != enemyTrainer1.ujimonTeam[0].name && ujimonInstances.ujimonArray[selectedUjimon].name != enemyTrainer1.ujimonTeam[1].name && ujimonInstances.ujimonArray[selectedUjimon].name != enemyTrainer1.ujimonTeam[2].name)
+            enemyTrainer1.ujimonTeam[i] = Ujimon(ujimonInstances.ujimonArray[selectedUjimon].healthPoints, ujimonInstances.ujimonArray[selectedUjimon].name, ujimonInstances.ujimonArray[selectedUjimon].imageAsset, ujimonInstances.ujimonArray[selectedUjimon].dead, ujimonInstances.ujimonArray[selectedUjimon].type)
+        }
+    }
+
+    fun createEnemyTrainer2Team(){
+        var selectedUjimon : Int
+        for( i in 0 until 3){
+            do {
+                selectedUjimon = Random.nextInt(0,9)
+            }
+            while (ujimonInstances.ujimonArray[selectedUjimon].name != enemyTrainer2.ujimonTeam[0].name && ujimonInstances.ujimonArray[selectedUjimon].name != enemyTrainer2.ujimonTeam[1].name && ujimonInstances.ujimonArray[selectedUjimon].name != enemyTrainer2.ujimonTeam[2].name && ujimonInstances.ujimonArray[selectedUjimon].name != enemyTrainer2.ujimonTeam[3].name )
+            enemyTrainer2.ujimonTeam[i] = Ujimon(ujimonInstances.ujimonArray[selectedUjimon].healthPoints, ujimonInstances.ujimonArray[selectedUjimon].name, ujimonInstances.ujimonArray[selectedUjimon].imageAsset, ujimonInstances.ujimonArray[selectedUjimon].dead, ujimonInstances.ujimonArray[selectedUjimon].type)
+        }
+    }
+
+    fun createEnemyTrainer3Team(){
+        var selectedUjimon : Int
+        for( i in 0 until 4){
+            do {
+                selectedUjimon = Random.nextInt(0,9)
+            }
+            while (ujimonInstances.ujimonArray[selectedUjimon].name != enemyTrainer3.ujimonTeam[0].name && ujimonInstances.ujimonArray[selectedUjimon].name != enemyTrainer3.ujimonTeam[1].name && ujimonInstances.ujimonArray[selectedUjimon].name != enemyTrainer3.ujimonTeam[2].name && ujimonInstances.ujimonArray[selectedUjimon].name != enemyTrainer3.ujimonTeam[3].name && ujimonInstances.ujimonArray[selectedUjimon].name != enemyTrainer3.ujimonTeam[4].name)
+            enemyTrainer3.ujimonTeam[i] = Ujimon(ujimonInstances.ujimonArray[selectedUjimon].healthPoints, ujimonInstances.ujimonArray[selectedUjimon].name, ujimonInstances.ujimonArray[selectedUjimon].imageAsset, ujimonInstances.ujimonArray[selectedUjimon].dead, ujimonInstances.ujimonArray[selectedUjimon].type)
+        }
     }
 }
